3da0209879fee3355368840bc80595a9a9e6ae92
AndrewZalesski <47337459+AndrewZalesski@users.noreply.github.com>
2024-11-14 18:54:06 -0500

    Delete models directory

--- models/User.js
+++
@@ -1,9 +0,0 @@
-// Database model for users
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-    walletAddress: String,
-    createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('User', userSchema);
