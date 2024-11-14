19777da2ecb976200fbee7e1889ed7a6efa51800
AndrewZalesski <47337459+AndrewZalesski@users.noreply.github.com>
2024-11-13 19:02:20 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 module.exports = {
     PORT: process.env.PORT || 3000,
     DB_URI: process.env.DB_URI || "mongodb://localhost:27017/ghostswap",
-    ALLOWED_ORIGINS: process.env.ALLOWED_ORIGINS || "https://kasper-3-0.webflow.io,https://kaspercoin.net",
+    ALLOWED_ORIGINS: process.env.ALLOWED_ORIGINS || "https://kasper-3-0.webflow.io,https://kaspercoin.net,https://ghostswap-69212846b3d2.herokuapp.com/",
     KASPA_API_URL: process.env.KASPA_API_URL || "https://api.kaspa.org",
     KASPLEX_API_URL: process.env.KASPLEX_API_URL || "https://api.kasplex.org/v1",
     RATE_LIMIT_WINDOW: parseInt(process.env.RATE_LIMIT_WINDOW, 10) || 15 * 60 * 1000,
