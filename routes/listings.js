a51ed07be1cf1dc587d10c12b2c1c5e13790f032
AndrewZalesski <47337459+AndrewZalesski@users.noreply.github.com>
2024-11-14 19:15:09 -0500

    Delete routes directory

--- routes/listings.js
+++
@@ -1,9 +0,0 @@
-
-const express = require('express');
-const router = express.Router();
-const listingsController = require('../controllers/listingsController');
-
-router.get('/', listingsController.getListings);
-router.post('/', listingsController.createListing);
-
-module.exports = router;
