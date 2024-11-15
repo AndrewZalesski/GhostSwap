23e58fdb089c89839f877f91d1f774fe115d0e8c
AndrewZalesski <47337459+AndrewZalesski@users.noreply.github.com>
2024-11-14 19:15:03 -0500

    Delete models directory

--- models/TokenStats.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const TokenStatsSchema = new mongoose.Schema({
-  ticker: { type: String, unique: true },
-  lastPurchasedPrice: { type: Number, required: true },
-  lastVolume: { type: Number, default: 0 },
-  lastTrades: { type: Number, default: 0 },
-});
-module.exports = mongoose.model('TokenStats', TokenStatsSchema);
