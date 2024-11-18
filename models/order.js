74802faac693139f3fb07ee8e1340ca5189d24d7
AndrewZalesski <47337459+AndrewZalesski@users.noreply.github.com>
2024-11-17 23:45:01 -0500

    Add files via upload

--- models/order.js
+++ models/order.js
@@ -4,11 +4,11 @@ const { v4: uuidv4 } = require('uuid');
 const orderSchema = new mongoose.Schema({
   id: { type: String, required: true, unique: true, default: uuidv4 },
   txJsonString: { type: String, required: true, unique: true },
-  ticker: { type: String, required: true },4
+  ticker: { type: String, required: true },
   amount: { type: Number, required: true }, // Amount in smallest units (e.g., sompi)
   uAmt: { type: Number, required: true }, // Total KAS amount in smallest units
   from: { type: String, required: true }, // User's address
-  psktData: { type: String, required: true }, // PSKT data if needed
+  psktData: { type: String, required: true }, // PSKT data associated with the transaction
   status: { type: String, enum: ['active', 'completed', 'canceled'], default: 'active' },
   createdAt: { type: Date, default: Date.now },
 });
